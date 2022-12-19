439d678b9b13c81765ad345cffe0883674724c3b
winterific <sam.winter@gmail.com>
2022-12-18 22:56:23 -0500

    Change player position

--- app/game.js
+++ app/game.js
@@ -5,7 +5,7 @@ export class Game {
     constructor(width, height) {
         this.w = width
         this.h = height
-        this.player = new Player(width/2 - PLAYER_WIDTH/2, height/2 - PLAYER_HEIGHT/2)
+        this.player = new Player(width*(3/4) - PLAYER_WIDTH/2, height*(3/4) - PLAYER_HEIGHT/2)
         this.enemies = [
             new Enemy(0, 0),
             new Enemy(0, 32),
